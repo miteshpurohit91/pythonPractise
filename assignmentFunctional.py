663f5d91e551206fa62a70046e291e97962e5c3c
miteshpurohit91 <purohit.mitesh91@gmail.com>
2024-05-29 16:11:40 +0530

    29-05-24 Commit

--- assignmentFunctional.py
+++ assignmentFunctional.py
@@ -4,3 +4,54 @@ from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 
 expectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+actuallist = []
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.maximize_window()
+#Implicite wait: global time out to the script: If anyelement not shown to page
+# it took max time 5 sec in current scenario , if element is visible in 2 sec.
+# it will imediat proceed without waiting for 5 sec
+#implicit wait took time for executing every line of code
+
+driver.implicitly_wait(10)
+
+#Class name: .search-keyword
+driver.find_element(By.CSS_SELECTOR,".search-keyword").send_keys("ber")
+
+#in Case of Sleep: It will wait till 2 second though element will display in 1 second or not.
+# Implicite wait is not work to Fetch list of elements from the page we required to give sleep time,
+# aA it took time to Load each values of list,
+time.sleep(2)
+#Find elements plural should : //div[@class='products']/div[1]
+results = driver.find_elements(By.XPATH,"//div[@class='products']/div")
+# count = len(results)
+# assert count > 0
+#Chaining Concept of Selenium
+
+for result in results:
+    actuallist.append(result.find_element(By.XPATH,"h4").text)
+    result.find_element(By.XPATH, "div/button ").click()
+
+assert expectedList == actuallist
+
+driver.find_element(By.CSS_SELECTOR,"img[alt='Cart']").click()
+driver.find_element(By.XPATH,"//button[text()='PROCEED TO CHECKOUT']").click()
+
+#SUM Validation
+prices = driver.find_elements(By.CSS_SELECTOR,"tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)          #48 +160 +
+
+print(sum)
+totalammount = int (driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+
+driver.find_element(By.CSS_SELECTOR,"input[class='promoCode']").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR,".promoBtn").click()
+print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
+
+# Explicitly wait is , to wait for
+
+time.sleep(4)
+
+
